4bfe2d560a3cf2fffce89ff98902119f80ce93be
Arthur Peronne <aperonne@ethz.ch>
2026-06-20 06:51:06 +0000

    AIagent automatic AE3dDilatedAttention

--- src/models/ae_models.py
+++ src/models/ae_models.py
@@ -7,12 +7,22 @@ import torch
 import torch.nn as nn
 
 
+class DilatedAttentionConv3DBlock(nn.Module):
+    """
+    3D dilated convolutional block with Squeeze-and-Excitation attention.
+    """
+    def __init__(self, in_channels, out_channels, dilation=1, downsample=True, reduction=16):
+        super().__init__()
+        self.conv_block = DilatedConv3DBlock(in_channels, out_channels, dilation=dilation, downsample=downsample)
+        self.se = SEBlock3D(out_channels, reduction)
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = self.se(x)
+        return x
+
+
 class DilatedConv3DBlock(nn.Module):
-    """
-    3D dilated convolutional block:
-    Conv3D (dilated) -> InstanceNorm3D -> ReLU -> Conv3D (dilated) -> InstanceNorm3D -> ReLU
-    Optionally followed by MaxPool3D for downsampling.
-    """
 
     def __init__(self, in_channels, out_channels, dilation=1, downsample=True):
         super().__init__()
@@ -233,6 +243,82 @@ class AutoEncoder3D_Attention(nn.Module):
         # Decoder
         self.fc_dec = nn.Linear(latent_dim, flattened_size)
 
+        self.initial_up = nn.ConvTranspose3d(128, 128, kernel_size=2, stride=2)
+
+        self.dec1 = UpConv3DBlock(128, 64)
+        self.dec2 = UpConv3DBlock(64, 32)
+        self.dec3 = UpConv3DBlock(32, 16)
+        self.dec4 = UpConv3DBlock(16, 8)
+
+        self.final_conv = nn.Conv3d(8, 1, kernel_size=3, stride=1, padding=1)
+        self.final_activation = nn.Sigmoid()
+
+    def encode(self, x):
+        x = self.enc1(x)
+        x = self.enc2(x)
+        x = self.enc3(x)
+        x = self.enc4(x)
+        x = self.bottleneck_conv(x)
+        x = self.final_down(x)
+        x = self.flatten(x)
+        x = self.dropout(x)
+        z = self.fc_enc(x)
+        return z
+
+    def decode(self, z):
+        x = self.fc_dec(z)
+        x = self.dropout(x)
+        x = x.view(-1, *self.feature_shape)
+        x = self.initial_up(x)
+        x = self.dec1(x)
+        x = self.dec2(x)
+        x = self.dec3(x)
+        x = self.dec4(x)
+        x = self.final_conv(x)
+        x = self.final_activation(x)
+        return x
+
+    def forward(self, x):
+        z = self.encode(x)
+        x_recon = self.decode(z)
+        return x_recon, z
+
+
+class AutoEncoder3D_DilatedAttention(nn.Module):
+    """
+    Dilated Convolutional Autoencoder with Squeeze-and-Excitation attention.
+    """
+    def __init__(self, latent_dim=20, input_shape=(1, 32, 128, 128), dropout_rate=0.0, reduction=16):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.input_shape = input_shape
+
+        # Encoder (Dilated + Attention)
+        self.enc1 = DilatedAttentionConv3DBlock(1, 8, dilation=1, downsample=True, reduction=reduction)
+        self.enc2 = DilatedAttentionConv3DBlock(8, 16, dilation=2, downsample=True, reduction=reduction)
+        self.enc3 = DilatedAttentionConv3DBlock(16, 32, dilation=4, downsample=True, reduction=reduction)
+        self.enc4 = DilatedAttentionConv3DBlock(32, 64, dilation=1, downsample=True, reduction=reduction)
+
+        self.bottleneck_conv = nn.Sequential(
+            nn.Conv3d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.InstanceNorm3d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.InstanceNorm3d(128),
+            nn.ReLU(inplace=True),
+        )
+
+        self.final_down = nn.Conv3d(128, 128, kernel_size=2, stride=2)
+
+        self.feature_shape = (128, 1, 4, 4)
+        flattened_size = 128 * 1 * 4 * 4  # 2048
+
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.fc_enc = nn.Linear(flattened_size, latent_dim)
+
+        # Decoder
+        self.fc_dec = nn.Linear(latent_dim, flattened_size)
         self.initial_up = nn.ConvTranspose3d(128, 128, kernel_size=2, stride=2)
 
         self.dec1 = UpConv3DBlock(128, 64)
@@ -709,8 +795,8 @@ def build_autoencoder(model_name, latent_dimensions, dropout_rate=0.0):
     elif model_name == "AE3dAttention":
         return AutoEncoder3D_Attention(latent_dim=latent_dimensions, dropout_rate=dropout_rate)
 
-    elif model_name == "AE3dDilated":
-        return AutoEncoder3D_Dilated(latent_dim=latent_dimensions, dropout_rate=dropout_rate)
+    elif model_name == "AE3dDilatedAttention":
+        return AutoEncoder3D_DilatedAttention(latent_dim=latent_dimensions, dropout_rate=dropout_rate)
 
     else:
         raise ValueError(f"Unknown model_name: {model_name}")
